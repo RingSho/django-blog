c2588f46edc6aae35e29541f0b0a2fcd0a76e7b6
林祥平 <s.0v0xx@outlook.jp>
2019-10-07 15:28:50 +0900

    add setting

--- project3/settings.py
+++ project3/settings.py
@@ -23,7 +23,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$x*w^nk*6(4jl-rr64uz!hc**dthxuag=t3*#sgm23cdxe$&)*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+if not DEBUG:
+    import django_heroku
+    django_heroku.settings(locals())
 
 ALLOWED_HOSTS = []
 
